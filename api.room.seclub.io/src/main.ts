352897352b001e19e6d085374f6dbef34f218718
Atom <albaraa456@gmail.com>
2023-05-08 20:41:47 +0300

    add ssl

--- api.room.seclub.io/src/main.ts
+++ api.room.seclub.io/src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
+  const httpsOptions = {
+    key: fs.readFileSync('./certificates/key.pem'),
+    cert: fs.readFileSync('./certificates/server.crt'),
+  };
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: '*',
     },
+    httpsOptions,
   });
   app.enableVersioning({
     type: VersioningType.URI,
