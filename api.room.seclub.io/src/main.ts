2f87d5401d100527040671fc2a568cee83246962
Atom <albaraa456@gmail.com>
2023-05-08 23:10:26 +0300

    use tls

--- api.room.seclub.io/src/main.ts
+++ api.room.seclub.io/src/main.ts
@@ -4,15 +4,15 @@ import { VersioningType } from '@nestjs/common';
 import * as fs from 'fs';
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: fs.readFileSync('./certificates/key.pem'),
-  //   cert: fs.readFileSync('./certificates/server.crt'),
-  // };
+  const httpsOptions = {
+    key: fs.readFileSync('./certificates/privkey.pem'),
+    cert: fs.readFileSync('./certificates/fullchain.pem'),
+  };
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: '*',
     },
-    // httpsOptions,
+    httpsOptions,
   });
   app.enableVersioning({
     type: VersioningType.URI,
